5a9c7b76e3cc6cad4db57dbdda2b5d376618c1bd
Victor <victor.wang647@hotmail.com>
2019-12-29 21:11:57 -0500

    temporary fix to allow drawing of labels

--- napari/layers/base/base.py
+++ napari/layers/base/base.py
@@ -550,6 +550,7 @@ class Layer(KeymapMixin, ABC):
         for d, p in zip(self.dims.displayed, self.position):
             coords[d] = p
         self.coordinates = tuple(coords)
+        print(coords, self.coordinates)
         self._value = self.get_value()
         self.status = self.get_message()
 
@@ -564,10 +565,10 @@ class Layer(KeymapMixin, ABC):
         full_scale = np.multiply(self.scale, self._scale_view)
         full_translate = np.add(self.translate, self._translate_view)
 
-        if not all(
-            isinstance(n, int) for n in self.coordinates
-        ):  # fixes bug where any element of self.coordinates is updated to a non-numeric value
-            self.coordinates = (0,) * len(self.coordinates)
+        # if not all(
+        #     isinstance(n, int) for n in self.coordinates
+        # ):  # fixes bug where any element of self.coordinates is updated to a non-numeric value
+        #     self.coordinates = (0,) * len(self.coordinates)
         full_coord = np.round(
             np.multiply(self.coordinates, full_scale) + full_translate
         ).astype(int)
